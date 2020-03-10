01ab4836fc336348e542177222b409821623826d
weslley braga <wesbragagt@gmail.com>
2020-03-09 22:49:49 -0500

    basics of the api working well

--- resolvers.js
+++ resolvers.js
@@ -2,11 +2,27 @@ const { promisify } = require('util')
 const fs = require('fs')
 const getAll = promisify(fs.readFile)
 
+function searchPokemons (str, arr) {
+  return arr.filter(element => element.Pokemon.toLowerCase().includes(str))
+}
+
 module.exports = {
   Query: {
-    async getPokemons (_, __, ___) {
+    async getPokemons (_, { search, type }, ___) {
       const data = await getAll('./pokemondb.json', 'utf8')
+      if (search) {
+        return searchPokemons(search, JSON.parse(data))
+      }
+      if (type) {
+        const typeMatch = e => e.Type1.toLowerCase() === type.toLowerCase() || e.Type2.toLowerCase() === type.toLowerCase()
+        return JSON.parse(data).filter(typeMatch)
+      }
       return JSON.parse(data)
+    },
+    async getPokemon (_, { name }, ___) {
+      const data = await getAll('./pokemondb.json', 'utf8')
+      const matchPokemonName = e => e.Pokemon.toLowerCase() === name.toLowerCase()
+      return JSON.parse(data).find(matchPokemonName)
     }
   }
 }
--- typeDefs.js
+++ typeDefs.js
@@ -1,10 +1,11 @@
 const { gql } = require('apollo-server')
 const typeDefs = gql`
     type Query {
-        getPokemons: [Pokemon!]
+        getPokemons(search: String, type: String): [Pokemon!]
+        getPokemon(name: String): Pokemon
     }
     type Pokemon{
-            Number: Int
+            Number: ID!
             Pokemon: String
             Type1: String
             Type2: String
